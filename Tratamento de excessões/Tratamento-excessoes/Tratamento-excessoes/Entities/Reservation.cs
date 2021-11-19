348fcd385d105e516c2cd4b578e63b9bf257a651
Guilherme Aires Pimenta de Macedo <guilhermeap.macedo20@gmail.com>
2021-11-19 13:52:57 -0300

    Tratamento de excessões

---
+++ Tratamento de excessões/Tratamento-excessoes/Tratamento-excessoes/Entities/Reservation.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Tratamento_excessoes.Entities.Exceptions;
+
+namespace Tratamento_excessoes.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+        }
+        public Reservation(int roomNumber, DateTime checkin, DateTime checkout)
+        {
+            if (checkout <= checkin)
+            {
+                throw new DomainException("Check-Out date must be after Check-In");
+            }
+            RoomNumber = roomNumber;
+            CheckIn = checkin;
+            CheckOut = checkout;
+        }
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+        public void UpdateDates(DateTime checkin, DateTime checkout)
+        {
+            DateTime now = DateTime.Now;
+            if (checkin < now || checkout < now)
+            {
+               throw new DomainException( "Dates for update must be future dates");
+            }
+            if (checkout <= checkin)
+            {
+                throw new DomainException("Check-Out date must be after Check-In");
+            }
+            CheckIn = checkin;
+            CheckOut = checkout;
+        }
+        public override string ToString()
+        {
+            return "Room: "
+                + RoomNumber
+                + ", Check-In: "
+                + CheckIn.ToString("dd/MM/yyyy")
+                + ", Check-Out: "
+                + CheckOut.ToString("dd/MM/yyyy")
+                + ", Duration: "
+                + Duration()
+                + " nights";
+        }
+    }
+}
---
+++ Tratamento de excessões/Tratamento-excessoes/Tratamento-excessoes/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Tratamento_excessoes.Entities;
+using Tratamento_excessoes.Entities.Exceptions;
+
+namespace Tratamento_excessoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try {
+                Console.Write("Room Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Check-In Date(dd/MM/yyyy): ");
+                DateTime dtCheckIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-Out Date(dd/MM/yyyy): ");
+                DateTime dtCheckOut = DateTime.Parse(Console.ReadLine());
+
+                Reservation r = new Reservation(number, dtCheckIn, dtCheckOut);
+                Console.WriteLine("Reservation: " + r);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation:  ");
+                Console.Write("Check-In Date (dd/MM/yyyy): ");
+                dtCheckIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-Out Date (dd/MM/yyyy): ");
+                dtCheckOut = DateTime.Parse(Console.ReadLine());
+
+                r.UpdateDates(dtCheckIn,dtCheckOut);
+                Console.WriteLine("Reservation: " + r);
+            }
+            catch(DomainException e )
+            {
+                Console.WriteLine("Error in reservation: " + e.Message);
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("Format error: " + e.Message);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Erro inesperado: " + e.Message);
+            }
+        }
+    }
+}
