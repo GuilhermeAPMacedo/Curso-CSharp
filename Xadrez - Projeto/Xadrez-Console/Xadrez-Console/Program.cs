f0884770eb4438f0fe9d25e9e21e01973cdcda74
Guilherme Aires Pimenta de Macedo <guilhermeap.macedo20@gmail.com>
2021-11-22 13:32:10 -0300

    Posição xadrez

--- Xadrez - Projeto/Xadrez-Console/Xadrez-Console/Program.cs
+++ Xadrez - Projeto/Xadrez-Console/Xadrez-Console/Program.cs
@@ -8,18 +8,10 @@ namespace Xadrez_Console
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
-                tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(Cor.Preta, tab), new Posicao(0, 7));
-                Tela.imprimirTabuleiro(tab);
-            }
-            catch(TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.toPosicao());
+            Console.ReadLine();
         }
     }
 }
